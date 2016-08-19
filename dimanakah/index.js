a4b1ac7fd998bf0adb1399be739eccc54cf68c34
Andrew <atandiawan@gmail.com>
2016-08-19 11:32:19 +0700

    Pertanyaan

---
+++ dimanakah/index.js
@@ -0,0 +1,14 @@
+var gambarhalaman = document.getElementById("ikon");
+var background = document.body.style.backgroundColor;
+console.log("text");
+console.log(background);
+
+
+var muncul = function (){
+	gambarhalaman.innerHTML = ' <iframe width="560" height="315" src="https://www.youtube.com/embed/cdmAEcXpZRk" frameborder="0" allowfullscreen></iframe>';
+}
+
+var gantibackground = function(){
+	console.log(background);
+	background = "yellow";
+}
